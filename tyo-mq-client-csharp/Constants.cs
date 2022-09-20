1eb4289a55cdc4e8b228416335a09c4d0e7932ef
Eric Tang <eric@tyo.com.au>
2022-09-20 10:55:27 +1000

    finished event conversion

--- tyo-mq-client-csharp/Constants.cs
+++ tyo-mq-client-csharp/Constants.cs
@@ -2,9 +2,15 @@ namespace tyo_mq_client_csharp;
 
     public static class Constants
     {
-        public static string botToken = "5759322943:AAEOc--4oUlRWt0KVPq3fe715KD1zuogzAc";
 
-        public static string botName = "SignalBot (by TYO Lab)";
 
-        public static string chatId = "-1001771259551";
+        public static readonly string ANONYMOUS = "ANONYMOUS";
+        public static readonly string EVENT_DEFAULT = "tyo-mq-mt-default";
+        public static readonly string SYSTEM = "TYO-MQ-SYSTEM";
+        public static readonly string ALL_PUBLISHERS = "TYO-MQ-ALL";
+
+        public static readonly string METHOD_BROADCAST = "broadcast";
+
+        // SERVER DEFAULTS
+        public static readonly int DEFAULT_PORT = 17352;
     }
